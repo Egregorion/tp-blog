9103a9215847f66322c8ab96f16e3a3de6c9fb8f
Egregorion <gregory.fourmaux@gmail.com>
2022-12-24 12:29:16 +0100

    ajout de quelques commentaires pour vous aider à vous y retrouver

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -1,17 +1,19 @@
 <?php 
-
+//le rôle du manager étant d'interagir avec la bdd, c'est ici que l'on va récupérer le fichier qui contient la fonction correspondante
 require_once './model/DBConnect.php';
+//nous allons transcrire les données récupérées sous la forme d'objets de la classe Post, nous devons donc inclure le fichier correspondant  
 require_once './model/classes/Post.php';
 
 class PostManager {
 
-    public static function getAllPosts() {
-        $dbh = dbconnect();
-        $query = ("SELECT * FROM t_post");
-        $stmt = $dbh->prepare($query);
-        $stmt->execute();
-        $posts = $stmt->fetchAll(PDO::FETCH_CLASS, 'Post');
-        return $posts;
+    //on va ensuite définir différentes méthodes très ciblées 
+    public static function getAllPosts() { //pour récupérer tous les articles
+        $dbh = dbconnect(); //on récupère notre objet PDO
+        $query = ("SELECT * FROM t_post"); //on écrit notre requête SQL
+        $stmt = $dbh->prepare($query); //on prépare la requête
+        $stmt->execute();//on l'execute 
+        $posts = $stmt->fetchAll(PDO::FETCH_CLASS, 'Post');//on transcris les résultats en objets de la class Post 
+        return $posts;//puis on renvoie les résultats
     }
 
     public static function getPostById($id) {
--- views/indexView.php
+++ views/indexView.php
@@ -1,13 +1,12 @@
 <?php 
 require_once 'partials/header.php';
-//var_dump($posts);
 ?>
 
 <section class="container mt-5">
     <div class="row">
         <?php foreach($posts as $post) { ?>
-            <div class="card col-12 col-md-4 col-lg-2">
-                <img src="..." class="card-img-top" alt="...">
+            <div class="card col-12 col-md-4 col-lg-3">
+                <img src="assets/img/<?php echo $post->getPicture() ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $post->getTitle() ?></h5>
                     <a href="single.php?id=<?php echo $post->getIdPost() ?>" class="btn btn-primary">Voir l'article</a>
